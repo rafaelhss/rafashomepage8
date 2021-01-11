d952c62a1bef85c747baa5cb19f0f3dbb21a5960
Rafaelhss <rafaelhss@gmail.com>
2021-01-11 16:12:14 -0300

    Create index.php

---
+++ alexa/index.php
@@ -0,0 +1,7 @@
+<html>
+<?php
+/* This will give an error. Note the output
+ * above, which is before the header() call */
+header('Location: https://www.amazon.com.br/rafael-Futebol-de-cinco/dp/B08KW6Q7GF/');
+exit;
+?>
