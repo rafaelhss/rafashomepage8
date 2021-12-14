7104d6cec80bda47fe80877e954b0e9dfdbf6344
Rafaelhss <rafaelhss@gmail.com>
2021-12-14 20:57:27 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,42 @@
 
 <?php 
 /*include_once("index.html"); */
-echo 'suave';
 ?>
+
+<center>
+<h1>Rafa's Home page</h1>
+</center>
+  <ul>
+    <li>
+      <a href="https://www.amazon.com.br/rafael-Futebol-de-cinco/dp/B08KW6Q7GF/" target="_blank"> <img src="https://images-na.ssl-images-amazon.com/images/I/51zoXib9epL.png" width="50px" height="50px"> </a>
+    </li>
+    <li>
+      <a href="https://www.amazon.com.br/rafael-Casca-Grossa/dp/B08DMYGHS1/" target="_blank"> <img src="https://images-na.ssl-images-amazon.com/images/I/51Vh833ua0L.png" width="50px" height="50px"> </a>
+    </li>
+  </ul>
+
+  <ul>
+    <li>
+      <a href="https://play.google.com/store/apps/details?id=com.frusoft.kicks" target="_blank">
+          <img src="https://play-lh.googleusercontent.com/e7RTBqXSyro4MYW10APFLrqeqX6JVLCYqybQEpQKopb8cBWFLCB0ioIH55_N199BtA=s180-rw" width="50px" height="50px"><img src="https://www.gstatic.com/android/market_images/web/play_prism_hlock_2x.png" width="40px" height="10px"> </a>
+    </li>
+    
+  </ul>
+  <ul>
+        	<li>
+      <a href="https://pacmary.herokuapp.com/">Pac Mary</a>
+      </li>
+        <li>
+            	<a href="https://crugame.herokuapp.com/">Cru Game</a>
+
+      </li>
+      <li>
+          <a href="https://lifegame3d.herokuapp.com/home.html">Vida de vaca</a>
+      </li>
+    </ul> 
+
+ 
+
+
+
+
