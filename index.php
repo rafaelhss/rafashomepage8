e11b22b94c837859d30d78770f58f288d252eb98
Rafaelhss <rafaelhss@gmail.com>
2021-12-16 19:28:00 -0300

    Add files via upload

--- index.php
+++ index.php
@@ -1,50 +1,42 @@
-
-<?php 
-/*include_once("index.html"); */
-?>
-
+<body bgcolor="cyan" background="https://cutewallpaper.org/21/geocities-backgrounds/backgrounds.jpg">
 <center>
-<h1>Rafa's Home page</h1>
+<font color="yellow" face="Comic Sans MS">
+    <h1>Rafa's Home page <img src="7upspot.gif"></h1>
+    </font>
+    <hr>
 </center>
   <ul>
     <li>
-      <a href="https://www.amazon.com.br/rafael-Futebol-de-cinco/dp/B08KW6Q7GF/" target="_blank"> <img src="https://images-na.ssl-images-amazon.com/images/I/51zoXib9epL.png" width="50px" height="50px"> </a>
+      <a href="https://www.amazon.com.br/rafael-Futebol-de-cinco/dp/B08KW6Q7GF/" target="_blank"> <img src="https://images-na.ssl-images-amazon.com/images/I/51zoXib9epL.png" width="100px" height="100px"> </a>
     </li>
     <li>
-      <a href="https://www.amazon.com.br/rafael-Casca-Grossa/dp/B08DMYGHS1/" target="_blank"> <img src="https://images-na.ssl-images-amazon.com/images/I/51Vh833ua0L.png" width="50px" height="50px"> </a>
+      <a href="https://www.amazon.com.br/rafael-Casca-Grossa/dp/B08DMYGHS1/" target="_blank"> <img src="https://images-na.ssl-images-amazon.com/images/I/51Vh833ua0L.png" width="100px" height="100px"> </a>
     </li>
   </ul>
 
   <ul>
     <li>
       <a href="https://play.google.com/store/apps/details?id=com.frusoft.kicks" target="_blank">
-          <img src="https://play-lh.googleusercontent.com/e7RTBqXSyro4MYW10APFLrqeqX6JVLCYqybQEpQKopb8cBWFLCB0ioIH55_N199BtA=s180-rw" width="50px" height="50px"><img src="https://www.gstatic.com/android/market_images/web/play_prism_hlock_2x.png" width="40px" height="10px"> </a>
+          <img src="https://play-lh.googleusercontent.com/e7RTBqXSyro4MYW10APFLrqeqX6JVLCYqybQEpQKopb8cBWFLCB0ioIH55_N199BtA=s180-rw" width="100px" height="100px"><img src="https://www.gstatic.com/android/market_images/web/play_prism_hlock_2x.png" width="60px" height="15px"> </a>
     </li>
     
   </ul>
-  <ul>
-        	<li>
-      <a href="https://pacmary.herokuapp.com/">Pac Mary</a>
-      </li>
-        <li>
-            	<a href="https://crugame.herokuapp.com/">Cru Game</a>
 
-      </li>
-      <li>
-          <a href="https://lifegame3d.herokuapp.com/home.html">Vida de vaca</a>
-      </li>
-    </ul> 
+ <br> <img src="line_dot.gif"> <a href="https://pacmary.herokuapp.com/">Pac Mary</a>
+<br> <img src="line_dot.gif"> <a href="https://crugame.herokuapp.com/">Cru Game</a>
+<br> <img src="line_dot.gif"> <a href="https://lifegame3d.herokuapp.com/home.html">Vida de vaca</a>
 
-<ul>
-        <li>
-          <a href="https://github.com/rafaelhss/Dossoccer">DosSoccer</a>
-      </li>
-      <li>
-          <a href="https://dossoccer.herokuapp.com">DosSoccer 3 - Mobile</a>
-      </li>
-</ul>
- 
-
-
-
-
+    <center>
+    <img src="dogruns.gif">
+    <br>
+    <img src="dog%20(1).jpg" height="200px">
+    <img src="dog%20(2).jpg" height="200px">
+    <img src="dog%20(3).jpg" height="200px">
+    </center>
+    
+    <center>
+        <img src="HeHeartlandCottage5668imagesWorkConstructAnim.gif">
+        <img src="counter.gif">
+        
+    </center>
+</body>
